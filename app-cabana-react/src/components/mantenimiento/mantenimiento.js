61b85821f42812db26453e1b18f01a4c13bbc343
KevinArce <iav.kevinarce@ufg.edu.sv>
2022-11-29 17:17:42 -0600

    added modulo Mantenimiento

--- app-cabana-react/src/components/mantenimiento/mantenimiento.js
+++ app-cabana-react/src/components/mantenimiento/mantenimiento.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+import "./mantenimiento.css";
+import group1 from "./assets/group1.svg";
+import eparrowUp1 from "./assets/eparrowUp1.svg";
+
 const Mantenimiento = () => {
   return (
-    <div>
-      <h1>Mantenimiento</h1>
+    <div className="mantenimiento">
+      <div className="flex-container">
+      </div>
+      <div className="flex-container-1">
+        <span className="mantenimiento-de-lot">Mantenimiento de Lotes</span>
+        <img className="group-1" src={group1} />
+      </div>
+      <select className="rectangle-20">
+        <option className="num-2022-2023">2022-2023</option>
+      </select>
+      <button className="rectangle-193">
+        <span className="seleccione-una-finca">Seleccione una finca</span>
+        <img className="eparrow-up-1" src={eparrowUp1} />
+      </button>
+      <span className="lotes">Lotes</span>
     </div>
   );
 };
