12bcc43ac65f811dddc38e8ee56a506cacc1ef32
nivekino <00100719@uca.edu.sv>
2022-12-05 14:15:40 -0600

    adding new screens and modify

--- app-cabana-react/src/components/home/home.js
+++ app-cabana-react/src/components/home/home.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./assets/home.css";
-import cisettingsFilled from "./assets/cisettingsFilled.svg";
 import carbonuserAvatarFi from "./assets/carbonuserAvatarFi.svg";
 import group1 from "./assets/group1.svg";
-import eparrowUp from "./assets/eparrowUp.svg";
 import serviceApi from "../../services/services";
+import Dropdown from "react-bootstrap/Dropdown";
+
 const Home = () => {
-  
-  const nomProv = localStorage.getItem("nomProv");
+  // const nomProv = localStorage.getItem("nomProv");
 
   //Use serviceApi to get the data from the server and display it in the class num-2021-2022
   serviceApi.get("index").then((response) => {
@@ -15,45 +14,62 @@ const Home = () => {
     console.log(response);
     if (response.data.error === 0) {
       return response.data;
-
     } else {
       return "No data";
     }
-  }
-  );
+  });
 
   return (
     <div className="portada">
-      <div className="flex-container"></div>
-      <img className="carbonuser-avatar-fi" src={carbonuserAvatarFi} />
-      <div className="flex-container-1">
-        <span className="maria-rosales">{localStorage.getItem("nomProv")}</span>
-        <img className="cisettings-filled" src={cisettingsFilled} />
+      <div className="container-user">
+        <img
+          className="carbonuser-avatar-fi"
+          src={carbonuserAvatarFi}
+          alt="group"
+        />
+        <span className="txt-user-profile">Maria Rosales</span>
       </div>
-      <div className="flex-container-2">
-        <span>Sección informativa</span>
-        <img className="group-1" src={group1} />
+
+      <div className="mt-3 flex-container-2">
+        <div className="mb-0">
+          <span className="txt-custom01">Sección informativa</span>
+          <img className="group-1" src={group1} alt="group" />
+        </div>
+
+        <span className="mb-2 actualizada">Actualizada</span>
       </div>
-      <span className="actualizada">Actualizada</span>
-      <span className="consulta-liquidacin">Consulta Liquidación, Corte:</span>
-      <div className="rectangle-20">
-        <span className="corte-3-desde-16-12">
-          Corte: 3 Desde: 16-12-2022 Hasta: 31-12-2022
+
+      <div className="mt-2 container-drop">
+        <span className="consulta-liquidacin">
+          Consulta Liquidación, Corte:
         </span>
-        <img className="eparrow-up" src={eparrowUp} />
+
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Seleccionar Corte
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item>Action</Dropdown.Item>
+            <Dropdown.Item>Another action</Dropdown.Item>
+            <Dropdown.Item>Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <button className="mt-2 rectangle-193">
+          <span className="ver-liquidaciones">Ver liquidaciones</span>
+        </button>
       </div>
-      <button className="rectangle-193">
-        <span className="ver-liquidaciones">Ver liquidaciones</span>
-      </button>
-      <div className="flex-container-3">
-        <span className="zafra">Zafra</span>
-        <span className="chart-title">Toneladas totales</span>
-      </div>
-      <div className="flex-container-4">
-        <span className="num-2021-2022">
-          {localStorage.getItem("data")} 
-        </span>
-        <span className="metric">370.57 TM</span>
+
+      <div className="mt-4 stats-container">
+        <div className="flex-container-3">
+          <span className="title-stat">Zafra</span>
+          <span className="num-stat">2021 - 2022</span>
+        </div>
+        <div className="flex-container-3">
+          <span className="title-stat">Toneladas totales</span>
+          <span className="num-stat">370.57 TM</span>
+        </div>
       </div>
     </div>
   );
