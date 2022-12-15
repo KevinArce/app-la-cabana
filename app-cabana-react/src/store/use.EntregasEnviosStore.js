da9088430b904cd8bb5353947c9523f1d299e34e
KevinArce <iav.kevinarce@ufg.edu.sv>
2022-12-15 15:55:50 -0600

    added zustand for EntregasEnviosStore front

---
+++ app-cabana-react/src/store/use.EntregasEnviosStore.js
@@ -0,0 +1,10 @@
+import create from "zustand/react";
+
+const EntregasEnviosStore = create((set) => ({
+    entregasEnvios: {fecmov: '', numenv: '', humeda: '', phrsquema: '', rendimr: '', tonel: '',
+    polca: '', Ncorte: '', codfinca: '', lote: '', NOMLOTE: '', NOMFINCA: '', estado: '',
+    },
+    setEntregasEnvios: (entregasEnvios) => set({ entregasEnvios }),
+}));
+
+export default EntregasEnviosStore;
